c6ec5023f5438897baf3dc6b8c6bc1a856740c87
ivan0124 <ivan0124.tw@gmail.com>
2016-05-12 13:13:56 +0800

    modify install.sh

--- install_MyHandler/install.sh
+++ install_MyHandler/install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-cp -f ./common_config.mk configure.ac Makefile.am ../CAgentIoT
-cp -f ./module_config.xml ../CAgentIoT/build/Standard/config/module/linux/
-cp -f ./agent_config.xml ../CAgentIoT/build/Standard/config/
-
-if [ "$1" = "-handler" ] ; then
-echo "install MyHandler"
-cp -f MyHandler.tar.gz ../CAgentIoT/Modules/
-cd ../CAgentIoT/Modules
-tar xzvf MyHandler.tar.gz
-fi
+#
+echo "install config file..."
+cp -f ./common_config.mk configure.ac Makefile.am ../../../
+cp -f ./module_config.xml ../../../build/Standard/config/module/linux/
+cp -f ./agent_config.xml ../../../build/Standard/config/
 
 echo "install config file done"
