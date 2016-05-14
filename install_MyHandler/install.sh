fb306e10765d6367f9293d53ef628f2856759e85
ivan0124 <ivan0124.tw@gmail.com>
2016-05-14 11:30:18 +0800

    change install.sh

--- install_MyHandler/install.sh
+++ install_MyHandler/install.sh
@@ -4,6 +4,5 @@ echo "install config file..."
 cp -f ./common_config.mk configure.ac Makefile.am ../../../
 cp -f ./module_config.xml ../../../build/Standard/config/module/linux/
 cp -f ./agent_config.xml ../../../build/Standard/config/
-cp -f libMqttDrv.so libAdvLog.so libAdvCJSON.so libAdvJSON.so ../../../Release/AgentService/module/
 cp -f libMqttDrv.so ../../../Release/AgentService/
 echo "install config file done"
