3f71c31e1c57693b58d95385a8abc66ae792941a
ivan0124 <ivan0124.tw@gmail.com>
2016-05-16 12:51:43 +0800

    change files

--- install_MyHandler/install.sh
+++ install_MyHandler/install.sh
@@ -4,7 +4,10 @@ echo "install config file..."
 cp -f ./common_config.mk configure.ac Makefile.am ../../../
 cp -f ./module_config.xml ../../../build/Standard/config/module/linux/
 cp -f ./agent_config.xml ../../../build/Standard/config/
-if [ -d ../../../Release/AgentServcie/ ] ; then
+ 
+cp -rf ../WISE_IOT/NetDevice/Mqtt/ ../../../Library/MQTTDrv
+#if [ -d ../../../Release/AgentServcie/ ] ; then
+    echo "install libMqttDrv.so..."
     cp -f libMqttDrv.so ../../../Release/AgentService/
-fi
+#fi
 echo "install config file done"
