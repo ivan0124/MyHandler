980ba2c5529790389ddcc758031899e2c8064bfb
ivan0124 <ivan0124.tw@gmail.com>
2016-05-16 16:51:00 +0800

    change install.sh contents

--- install_MyHandler/install.sh
+++ install_MyHandler/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 #
 echo "install config file..."
 cp -f ./common_config.mk configure.ac Makefile.am ../../../
@@ -7,9 +7,9 @@ cp -f ./agent_config.xml ../../../build/Standard/config/
  
 cp -rf ../WISE_IOT/NetDevice/Mqtt/ ../../../Library/MQTTDrv
 
-echo "install libAdvCJSON.la, libAdvCJSON.so..."
-cp -f ./libAdvCJSON.la ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.la
-cp -f ./libAdvCJSON.so ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.so
+#echo "install libAdvCJSON.la, libAdvCJSON.so..."
+#cp -f ./libAdvCJSON.la ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.la
+#cp -f ./libAdvCJSON.so ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.so
 #if [ -d ../../../Release/AgentServcie/ ] ; then
     #echo "install libMqttDrv.so..."
     #cp -f libMqttDrv.so ../../../Release/AgentService/
