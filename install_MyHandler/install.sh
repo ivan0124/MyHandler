e319ec6ad919eb0f56a91519a25874ed1e151f01
ivan0124 <ivan0124.tw@gmail.com>
2016-05-16 13:45:05 +0800

    change files

--- install_MyHandler/install.sh
+++ install_MyHandler/install.sh
@@ -6,8 +6,12 @@ cp -f ./module_config.xml ../../../build/Standard/config/module/linux/
 cp -f ./agent_config.xml ../../../build/Standard/config/
  
 cp -rf ../WISE_IOT/NetDevice/Mqtt/ ../../../Library/MQTTDrv
+
+echo "install libAdvCJSON.la, libAdvCJSON.so..."
+cp -f ./libAdvCJSON.la ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.la
+cp -f ./libAdvCJSON.so ../../../Library/Log/AdvLog/Tools/AdvJSON/src/libAdvCJSON.so
 #if [ -d ../../../Release/AgentServcie/ ] ; then
-    echo "install libMqttDrv.so..."
-    cp -f libMqttDrv.so ../../../Release/AgentService/
+    #echo "install libMqttDrv.so..."
+    #cp -f libMqttDrv.so ../../../Release/AgentService/
 #fi
 echo "install config file done"
