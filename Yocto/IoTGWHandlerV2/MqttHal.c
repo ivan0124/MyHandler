e833df11e52240b89e7336924748c3b98f0735d0
ivan0124 <ivan0124.tw@gmail.com>
2016-06-08 14:07:30 +0800

    refactory function name

--- Yocto/IoTGWHandlerV2/MqttHal.c
+++ Yocto/IoTGWHandlerV2/MqttHal.c
@@ -122,7 +122,7 @@ int GetSusiCommand(JSONode *json, int* piSusiCommand){
     return 0;
 }
 
-int ReplyGetSetRequest(char* ptopic, JSONode *json, int cmdID){
+int ReplySensorHubGetSetRequest(char* ptopic, JSONode *json, int cmdID){
 
     char nodeContent[MAX_JSON_NODE_SIZE];
     char sessionID[256]={0};
@@ -447,7 +447,7 @@ int MqttHal_Message_Process(const struct mosquitto_message *message)
 #endif
 
 #if 1
-                    if ( ReplyGetSetRequest(message->topic,json, IOTGW_GET_SENSOR_REPLY) < 0){
+                    if ( ReplySensorHubGetSetRequest(message->topic,json, IOTGW_GET_SENSOR_REPLY) < 0){
                         printf("[%s][%s] Reply Get Sensor Request FAIL !!!\n", __FILE__, __func__);
                         JSON_Destory(&json);
                         return -1;
@@ -476,7 +476,7 @@ int MqttHal_Message_Process(const struct mosquitto_message *message)
 #endif
 
 #if 1
-                    if ( ReplyGetSetRequest(message->topic,json, IOTGW_SET_SENSOR_REPLY) < 0){
+                    if ( ReplySensorHubGetSetRequest(message->topic,json, IOTGW_SET_SENSOR_REPLY) < 0){
                         printf("[%s][%s] Reply Set Sensor Request FAIL !!!\n", __FILE__, __func__);
                         JSON_Destory(&json);
                         return -1;
