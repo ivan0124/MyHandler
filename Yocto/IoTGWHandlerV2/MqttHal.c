e0a643c5b17be5c2109c2eb2f4da0b8063b12d3d
ivan0124 <ivan0124.tw@gmail.com>
2016-06-24 20:49:20 +0800

    use strtok_r instead of strtok

--- Yocto/IoTGWHandlerV2/MqttHal.c
+++ Yocto/IoTGWHandlerV2/MqttHal.c
@@ -785,12 +785,13 @@ int DisconnectSensorHubInVirtualGateway(struct node* head, char* VirtualGatewayU
     {
         if (strcmp(r->virtualGatewayDevID, VirtualGatewayUID) == 0){
             char tmp[1024]={0};
+            char* pSave = NULL;
             strcpy(tmp,r->connectivitySensorHubList);
-	    char *SensorHubUID = strtok(tmp, ",");
+	    char *SensorHubUID = strtok_r(tmp, ",", &pSave);
 	    while(SensorHubUID != NULL)
 	    {
                 DisconnectToRMM_SensorHub(SensorHubUID);
-		SensorHubUID = strtok(NULL, ",");
+		SensorHubUID = strtok_r(NULL, ",", &pSave);
 	    }
         }
         //
