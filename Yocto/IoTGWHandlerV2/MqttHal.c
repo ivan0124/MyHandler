73cfe70037d23feda84e41faf4851c9ce44deeb7
ivan0124 <ivan0124.tw@gmail.com>
2016-07-14 13:51:58 +0800

    use adv logs

--- Yocto/IoTGWHandlerV2/MqttHal.c
+++ Yocto/IoTGWHandlerV2/MqttHal.c
@@ -990,12 +990,14 @@ int MqttHal_Message_Process(const struct mosquitto_message *message)
                         /*Re-connect device*/ 
                          ADV_C_DEBUG(COLOR_YELLOW,"[%s][%s]\033[33m #Re-Connect# \033[0m\n", __FILE__, __func__);
                         app_os_mutex_unlock(&g_NodeListMutex);
-                        //char mydata[512]={"{\"susiCommData\":{\"commCmd\":125,\"handlerName\":\"general\",\"response\":{\"statuscode\":4,\"msg\": \"Reconnect\"}}}"};
+
                         if ( GetRequestCmd(IOTGW_RECONNECT, request_cmd) < 0){
                             ADV_C_ERROR(COLOR_RED, "[%s][%s]GATEWAY_HEART_BEAT: GetRequestCmd FAIL.\n",__FILE__,__func__);
                         }
                         else{
-                            SendRequestToWiseSnail(gateway_devID,request_cmd);
+                            if ( SendRequestToWiseSnail(gateway_devID,request_cmd) != 0){
+                                ADV_C_ERROR(COLOR_RED, "[%s][%s]GATEWAY_HEART_BEAT: SendRequestToWiseSnail FAIL.\n",__FILE__,__func__);
+                            }
                         }
                         JSON_Destory(&json);
                         return;
@@ -1680,7 +1682,7 @@ int SendRequestToWiseSnail(char *macAddr, const char *pData)
             ADV_C_ERROR(COLOR_RED, "[%s][%s] mosquitto_publish error code=%d\n", __FILE__, __func__, rc);
         }
 
-	return 0;
+	return rc;
 }
 
 int BuildNodeList_GatewayCapabilityInfoWithData(struct node* head, char* pResult){
