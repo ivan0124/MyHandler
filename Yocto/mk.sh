f4816f7c252e04ae08711a770ed073b53bbfc277
ivan0124 <ivan0124.tw@gmail.com>
2016-07-19 10:57:52 +0800

    add config files

--- Yocto/mk.sh
+++ Yocto/mk.sh
@@ -11,12 +11,15 @@ if [ "$1" == "v1" ] ; then
     cd /mnt/MyHandler/Yocto/
 else
 echo "build IoTGWHandlerV2 version"
-    rm -rf ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
+    #rm -rf ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
     cp -rf ./IoTGWHandlerV2 ~ivan/CAgentIoT/Modules/
     cd ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
+    make clean
     make
-    cp ./IoTSensorHandlerV2.so.3.1.32.5653 ~ivan/CAgentIoT/Release/AgentService/module/
+    cp ./IoTSensorHandlerV2.so.3.2.5.5990 ~ivan/CAgentIoT/Release/AgentService/module/
     cd /mnt/MyHandler/Yocto/
     echo "build IoTGWHandlerV2 version done"
 fi
 cp -rf ./module_config.xml ~ivan/CAgentIoT/Release/AgentService/module/
+cp -rf ./agent_config.xml ~ivan/CAgentIoT/Release/AgentService/
+#cp -f ./IoTGWHandlerV2/mqtt_sub.conf ~ivan/CAgentIoT/Release/AgentService/
