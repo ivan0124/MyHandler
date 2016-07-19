083bda2655ae49a0ad5c01203f53399df85af6c9
ivan0124 <ivan0124.tw@gmail.com>
2016-07-19 11:54:54 +0800

    update mk.sh

--- Yocto/mk.sh
+++ Yocto/mk.sh
@@ -10,16 +10,24 @@ if [ "$1" == "v1" ] ; then
     cp ./IoTSensorHandler.so.3.1.32.5653 ~ivan/CAgentIoT/Release/AgentService/module/IoTSensorHandlerV2.so.3.1.30.5434
     cd /mnt/MyHandler/Yocto/
 else
-echo "build IoTGWHandlerV2 version"
     #rm -rf ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
-    cp -rf ./IoTGWHandlerV2 ~ivan/CAgentIoT/Modules/
-    cd ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
-    make clean
-    make
-    cp ./IoTSensorHandlerV2.so.3.2.5.5990 ~ivan/CAgentIoT/Release/AgentService/module/
-    cd /mnt/MyHandler/Yocto/
-    echo "build IoTGWHandlerV2 version done"
+    
+    if [ "$2" != "3310" ] ; then
+        echo "build IoTGWHandlerV2 version"
+        cp -rf ./IoTGWHandlerV2 ~ivan/CAgentIoT/Modules/
+        cd ~ivan/CAgentIoT/Modules/IoTGWHandlerV2/
+        make clean
+        make
+        cp ./IoTSensorHandlerV2.so.3.2.5.5990 ~ivan/CAgentIoT/Release/AgentService/module/
+        cd /mnt/MyHandler/Yocto/
+        echo "build IoTGWHandlerV2 version done"
+    fi
 fi
+
 cp -rf ./module_config.xml ~ivan/CAgentIoT/Release/AgentService/module/
 cp -rf ./agent_config.xml ~ivan/CAgentIoT/Release/AgentService/
-#cp -f ./IoTGWHandlerV2/mqtt_sub.conf ~ivan/CAgentIoT/Release/AgentService/
+
+if [ "$2" == "3310" ] ; then
+    cp -rf ~ivan/CAgentIoT/Release/AgentService /var/nfsshare
+    echo "copy AgentService to /var/nfsshare done."
+fi
