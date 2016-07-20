1a36fe350b8959ed9734744d4287c9f812bfd741
ivan0124 <ivan0124.tw@gmail.com>
2016-07-20 15:45:43 +0800

    add mk.sh

---
+++ CAgetnIoT3.2/mk.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#
+    
+echo "build IoTGWHandlerV2 version"
+cp Makefile.am configure.ac ~ivan/CAgentIoT3.2
+cp -rf ./myTest ~ivan/CAgentIoT3.2/Modules/
+#make clean
+#make
+echo "build IoTGWHandlerV2 version done"
+
+
