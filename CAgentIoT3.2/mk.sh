f73ecf70e393a23be49c97d9891f9cdc6a22b076
ivan0124 <ivan0124.tw@gmail.com>
2016-07-22 14:14:38 +0800

    chnage myTest name to IoTGWHandlerV2

--- CAgentIoT3.2/mk.sh
+++ CAgentIoT3.2/mk.sh
@@ -1,34 +1,41 @@
 #/bin/bash
 #
-    
-echo "build myTest module"
+
+my_module=IoTGWHandlerV2    
+echo "build IoTGWHandlerV2 module"
 cp Makefile.am configure.ac ~ivan/CAgentIoT3.2
-rm -rf ~ivan/CAgentIoT3.2/Modules/myTest/
-cp -rf ./myTest ~ivan/CAgentIoT3.2/Modules/
+#rm -rf ~ivan/CAgentIoT3.2/Modules/myTest/
+rm -rf ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/
+cp -rf /mnt/MyHandler/Yocto/IoTGWHandlerV2 ~ivan/CAgentIoT3.2/Modules
+cp -rf ./myTest/Makefile.am ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/
+cp -rf ./myTest/common.c ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/
+cp -rf ./myTest/common.h ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/
+cp -rf ./myTest/platform.h ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/
+#cp -rf ./myTest ~ivan/CAgentIoT3.2/Modules/
 cd ~ivan/CAgentIoT3.2/
 
-#generating ./myTest/Makefile.in
+#generating ./IoTGWHandlerV2/Makefile.in
 automake
 #generating ./CAgentIoT3.2/configure
 autoconf
 
-#build myTest module
+#build IoTGWHanlerV2 module
 make clean
 make
 
 echo "build myTest module done"
-cp -f ~ivan/CAgentIoT3.2/Modules/myTest/.libs/IoTSensorHandlerV2-3.2.6.551.so ~ivan/CAgentIoT3.2/Release/AgentService/module/
+cp -f ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/.libs/IoTSensorHandlerV2-3.2.6.551.so ~ivan/CAgentIoT3.2/Release/AgentService/module/
 
 cd ~ivan/CAgentIoT3.2/Release/AgentService/module/
 ln -s IoTSensorHandlerV2-3.2.6.551.so IoTSensorHandlerV2.so
 
-cp -f ~ivan/CAgentIoT3.2/Modules/myTest/mqtt_sub.conf ~ivan/CAgentIoT3.2/Release/AgentService/
+cp -f ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/mqtt_sub.conf ~ivan/CAgentIoT3.2/Release/AgentService/
 
 cp -rf /mnt/MyHandler/Yocto/module_config.xml ~ivan/CAgentIoT3.2/Release/AgentService/module/
 cp -rf /mnt/MyHandler/Yocto/agent_config.xml ~ivan/CAgentIoT3.2/Release/AgentService/
-#list build myTest result
+#list build IoTGWHandlerV2 result
 echo "List build myTest module result..."
-ls -al ~ivan/CAgentIoT3.2/Modules/myTest/.libs/
+ls -al ~ivan/CAgentIoT3.2/Modules/IoTGWHandlerV2/.libs/
 
 #cd /mnt/MyHandler/CAgentIoT3.2
 
