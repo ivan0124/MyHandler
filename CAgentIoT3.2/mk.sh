0cd56923fbed5a4cf724be057fbfaa0110180d57
ivan0124 <ivan0124.tw@gmail.com>
2016-07-22 11:04:48 +0800

    CAgentIoT3.2 IoTGWHandlerV2 build ok

--- CAgentIoT3.2/mk.sh
+++ CAgentIoT3.2/mk.sh
@@ -17,6 +17,10 @@ make clean
 make
 
 echo "build myTest module done"
+cp -f ~ivan/CAgentIoT3.2/Modules/myTest/.libs/libIoTSensor-3.2.6.551.so ~ivan/CAgentIoT3.2/Release/AgentService/module/
+
+cp -f ~ivan/CAgentIoT3.2/Modules/myTest/mqtt_sub.conf ~ivan/CAgentIoT3.2/Release/AgentService/
+
 #list build myTest result
 echo "List build myTest module result..."
 ls -al ~ivan/CAgentIoT3.2/Modules/myTest/.libs/
